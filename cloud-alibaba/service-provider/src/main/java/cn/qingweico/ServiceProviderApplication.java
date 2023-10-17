4d58ba55d7ddb9b970fafd6dc1d5c35148e1e80b
zqw <zhouqw@tranzvision.com.cn>
2023-10-17 17:34:59 +0800

    2023-10-17

---
+++ cloud-alibaba/service-provider/src/main/java/cn/qingweico/ServiceProviderApplication.java
@@ -0,0 +1,18 @@
+package cn.qingweico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zqw
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProviderApplication.class, args);
+    }
+
+}
